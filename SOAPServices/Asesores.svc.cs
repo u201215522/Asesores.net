7fbc5eb26df0d322394abd9656504b181e2a9188
u201215522 <artuis@gmail.com>
2013-09-17 01:35:12 -0500

    Primer commit con el proyecto funcionando localmente

---
+++ SOAPServices/Asesores.svc.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using SOAPServices.Persistencia;
+using SOAPServices.Dominio;
+
+namespace SOAPServices
+{
+    public class Asesores : IAsesores
+    {
+        private AsesorDAO asesorDAO = null;
+        private AsesorDAO AsesorDAO
+        {
+            get
+            {
+                if (asesorDAO == null)
+                    asesorDAO = new AsesorDAO();
+                return asesorDAO;
+            }
+        }
+        private SedeDAO sedeDAO = null;
+        private SedeDAO SedeDAO
+        {
+            get
+            {
+                if (sedeDAO == null)
+                    sedeDAO = new SedeDAO();
+                return sedeDAO;
+            }
+        }
+
+        public Asesor CrearAsesor(string nombre, string correo, int sede)
+        {
+            Sede sedeExistente = SedeDAO.Obtener(sede);
+            Asesor asesorACrear = new Asesor()
+            {
+                Nombre = nombre,
+                Correo = correo,
+                Sede = sedeExistente
+            };
+            return AsesorDAO.Crear(asesorACrear);
+        }
+
+        public Asesor ObtenerAsesor(int codigo)
+        {
+            return AsesorDAO.Obtener(codigo);
+        }
+
+        public Asesor ModificarAsesor(int codigo, string nombre, string correo, int sede)
+        {
+            Sede sedeExistente = SedeDAO.Obtener(sede);
+            Asesor asesorAModificar = new Asesor()
+            {
+                Codigo = codigo,
+                Nombre = nombre,
+                Correo = correo,
+                Sede = sedeExistente
+            };
+            return AsesorDAO.Modificar(asesorAModificar);
+        }
+
+        public void EliminarAsesor(int codigo)
+        {
+            Asesor asesorExistente = AsesorDAO.Obtener(codigo);
+            AsesorDAO.Eliminar(asesorExistente);
+        }
+
+        public List<Asesor> ListarAsesores()
+        {
+            return AsesorDAO.ListarTodos().ToList();
+        }
+    }
+}
---
+++ SOAPServices/Persistencia/ConexionUtil.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SOAPServices.Persistencia
+{
+    public class ConexionUtil
+    {
+        public static string ObtenerCadena()
+        {
+            return "Data Source=7c054cfb-6a46-45ae-bcac-a23c005429a7.sqlserver.sequelizer.com;Initial Catalog=db7c054cfb6a4645aebcaca23c005429a7;User Id=ylmypszgjlgmqvbn;Password=7Fn56SWkYz6kFYXDM6MejeZ4FaHnZJqG6LUZBLwnLsgkUTQELRYRn7v3Nsw5iJsp;";
+        }
+    }
+}
